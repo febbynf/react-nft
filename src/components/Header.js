b6071696c373b39464e0411d31d1c1dfd5eb36dd
febbynf <febbynurfitriyani1208@gmail.com>
2022-03-03 15:55:04 +0700

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
-  return <h2>I'm The Header</h2>
+  return (
+    <div className='header'>
+        <div className='logoContainer'>
+            {/* <img src={punkLogo} className='punkLogo' alt='' /> */}
+        </div>
+    </div> 
+  )
 }
 
 export default Header
