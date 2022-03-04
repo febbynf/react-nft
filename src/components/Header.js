529dcb52c4c6de14373af81c6785153a1df04685
febbynf <febbynurfitriyani1208@gmail.com>
2022-03-04 11:56:59 +0700

    edit header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
 
 const Header = () => {
   return (
     <div className='header'>
         <div className='logoContainer'>
-            {/* <img src={punkLogo} className='punkLogo' alt='' /> */}
+            <img src={punkLogo} className='punkLogo' alt='' />
+        </div>
+
+        <div className='searchBar'>
+            <div className='searchIconContainer'>
+                <img src={searchIcon} />
+                <input className='searchInput' placeholder='Collection, Item or User ..' />
+            </div>
         </div>
     </div> 
   )
